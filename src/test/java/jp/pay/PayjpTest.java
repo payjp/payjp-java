f61d9acfbafd8485891ad9ed2fc9eaab587cdbbe
natsuki-yamanaka <natsuki-yamanaka@binc.jp>
2022-06-07 14:24:05 +0900

    Update src/test/java/jp/pay/PayjpTest.java
    Co-authored-by: Daiki Arai <darai0512@yahoo.co.jp>

--- src/test/java/jp/pay/PayjpTest.java
+++ src/test/java/jp/pay/PayjpTest.java
@@ -182,7 +182,7 @@ public class PayjpTest extends BasePayjpTest {
 
 	@Test(expected=APIException.class)
 	public void testAPIExceptionWithHtmlResponse() throws PayjpException {
-		stubNetwork(Customer.class, 504, "<html>504 Gateway Time-out</html>");
+		stubNetwork(Charge.class, 504, "<html>504 Gateway Time-out</html>");
 		Charge.create(defaultChargeParams);
 	}
 
